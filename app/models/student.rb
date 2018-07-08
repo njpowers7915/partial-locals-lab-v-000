fb21106d271824d51c85ebc837d714e525bfd1ac
Nicholas Powers <njpowers7915@gmail.com>
2018-07-08 01:34:06 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,6 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(name)
-      Student.all.search(name).map {|s| s.name}
+      Student.search(name).map {|s| s.name}
   end
 end
